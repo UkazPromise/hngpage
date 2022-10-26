d5302b45a441c390f8f688de2a66c1e54ff5e133
UkazPromise <77250261+UkazPromise@users.noreply.github.com>
2022-10-26 15:07:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to HNG Internship 9.0 👍</h1>
+      <p>HNG is a virtual internship programme meaning we are going to operate purely online. You will recieve tasks <br/>that mirrors the tasks 
+      you will get in the real-world. Every week new task, different things, this task will test your technicals ability, we are going to test your team<br/>
+      team work. There will be design for you to always be able to do them by reading reading up things online but they are designed to be a<br/> little challenging
+      You will be invited to slack and every week you are going to recieve a new task, you will have to complete that task<br/> successfully in order to move on to 
+      the next stage. There are ten (10) stages. the 10th and final stage are for those who become the HNG's finalist.<br/> the only thing we promise is that we
+      are going to give you a very nice T-Shirt, but normally good things happen to the finalist. You are now in the LMS meaning<br/> this is the place
+      we are going to see different tasks that you would normally get, you can have the selected task that you wanna do,<br/> once you have selected the track you can
+      select the courses be sure to click the red button at the top to do that. There are already tasks you can start on<br/> your own once you have selected
+      you track and courses go in and attempt them. After you're in slack or the programme start, you can continue to submit<br/> the solutions, so if you're
+      struggling to do it alone then wait till the programme starts, you are gonna have mentors and peers around you that<br/> will help you to complete the 
+      task successfully. You are not in slack you are in LMS, you are going to recieve an email that will invite you to slack<br/> Slack is a chatting programme
+      and when you are on slack you can chat with everyone one and have lots of interactions.
+      But if you've not registered on Learning Management System (LMS), please signup through this <button><a href="https://hngi9.zuriboard.com/">link</a></button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
